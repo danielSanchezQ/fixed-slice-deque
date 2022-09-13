c7a873ccf75d278dc58c0c7c514ee54633d929da
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2022-09-13 09:42:18 +0200

    Remaining methods docs

--- src/lib.rs
+++ src/lib.rs
@@ -152,6 +152,12 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.len()
     }
 
+    /// Buffer capacity
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
     /// Extracts a slice containing the entire deque.
     #[inline]
     pub fn as_slice(&self) -> &[T] {
@@ -403,21 +409,94 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.pop_back()
     }
 
+    /// Shortens the deque by removing excess elements from the front.
+    /// At most `capacity` is removed (`len.min(self.capacity)`)
+    ///
+    /// If `len` is greater than the FixedSliceDeque's all items will be removed
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![5, 10, 15];
+    /// assert_eq!(deq, [5, 10, 15]);
+    /// deq.truncate_front(1);
+    /// assert_eq!(deq, [15]);
+    /// # }
+    /// ```
     #[inline]
     pub fn truncate_front(&mut self, len: usize) {
         self.buffer.truncate_front(len.min(self.capacity));
     }
 
+    /// Shortens the deque by removing excess elements from the back.
+    /// At most `capacity` is removed (`len.min(self.capacity)`)
+    ///
+    /// If `len` is greater than the FixedSliceDeque's all items will be removed
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![5, 10, 15];
+    /// assert_eq!(deq, [5, 10, 15]);
+    /// deq.truncate_back(1);
+    /// assert_eq!(deq, [5]);
+    /// # }
+    /// ```
     #[inline]
     pub fn truncate_back(&mut self, len: usize) {
         self.buffer.truncate_back(len.min(self.capacity));
     }
 
+    /// Removes all values from the deque.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1];
+    /// assert!(!deq.is_empty());
+    /// deq.clear();
+    /// assert!(deq.is_empty());
+    /// # }
     #[inline]
     pub fn clear(&mut self) {
         self.buffer.clear()
     }
 
+    /// Creates a draining iterator that removes the specified range in the
+    /// deque and yields the removed items.
+    ///
+    /// Note 1: The element range is removed even if the iterator is only
+    /// partially consumed or not consumed at all.
+    ///
+    /// Note 2: It is unspecified how many elements are removed from the deque
+    /// if the `Drain` value is leaked.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the starting point is greater than the end point or if
+    /// the end point is greater than the length of the deque.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 3];
+    /// let u: Vec<_> = deq.drain(1..).collect();
+    /// assert_eq!(deq, &[1]);
+    /// assert_eq!(u, &[2, 3]);
+    ///
+    /// // A full range clears the deque
+    /// deq.drain(..);
+    /// assert_eq!(deq, &[]);
+    /// # }
+    /// ```
     #[inline]
     pub fn drain<R>(&mut self, range: R) -> slice_deque::Drain<T>
     where
@@ -426,11 +505,35 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.drain(range)
     }
 
+    /// Extracts a the inner [`SliceDeque`] buffer, consuming the [`FixedSliceDeque`]
     #[inline]
     pub fn into_inner(self) -> SliceDeque<T> {
         self.buffer
     }
 
+    /// Inserts an `element` at `index` within the deque, shifting all elements
+    /// with indices greater than or equal to `index` towards the back, keeping the deque max capacity.
+    /// If the deque `if_full` the back-most element is returned.
+    ///
+    /// Element at index 0 is the front of the queue.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `index` is greater than deque's length
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq!['a', 'b', 'c'];
+    /// assert_eq!(deq, &['a', 'b', 'c']);
+    ///
+    /// let c = deq.insert(1, 'd');
+    /// assert_eq!(deq, &['a', 'd', 'b']);
+    /// assert_eq!(c, Some('c'));
+    /// # }
+    /// ```
     #[inline]
     pub fn insert(&mut self, index: usize, element: T) -> Option<T> {
         let is_full = self.is_full();
@@ -442,11 +545,43 @@ impl<T> FixedSliceDeque<T> {
         ret
     }
 
+    /// Removes and returns the element at position `index` within the deque.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `index` is out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 3, 4, 5];
+    /// assert_eq!(deq.remove(1), 2);
+    /// assert_eq!(deq, [1, 3, 4, 5]);
+    /// # }
+    /// ```
     #[inline]
     pub fn remove(&mut self, index: usize) -> T {
         self.buffer.remove(index)
     }
 
+    /// Retains only the elements specified by the predicate.
+    ///
+    /// That is, remove all elements `e` such that `f(&e)` returns `false`.
+    /// This method operates in place and preserves the order of the
+    /// retained elements.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 3, 4];
+    /// deq.retain(|&x| x % 2 == 0);
+    /// assert_eq!(deq, [2, 4]);
+    /// # }
+    /// ```
     #[inline]
     pub fn retain<F>(&mut self, f: F)
     where
@@ -455,6 +590,22 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.retain(f);
     }
 
+    /// Removes all but the first of consecutive elements in the deque that
+    /// resolve to the same key.
+    ///
+    /// If the deque is sorted, this removes all duplicates.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![10, 20, 21, 30, 20];
+    ///
+    /// deq.dedup_by_key(|i| *i / 10);
+    /// assert_eq!(deq, [10, 20, 30, 20]);
+    /// # }
+    /// ```
     #[inline]
     pub fn dedup_by_key<F, K>(&mut self, key: F)
     where
@@ -464,6 +615,29 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.dedup_by_key(key);
     }
 
+    /// Removes all but the first of consecutive elements in the deque
+    /// satisfying a given equality relation.
+    ///
+    /// The `same_bucket` function is passed references to two elements from
+    /// the deque, and returns `true` if the elements compare equal, or
+    /// `false` if they do not. The elements are passed in opposite order
+    /// from their order in the deque, so if `same_bucket(a, b)` returns
+    /// `true`, `a` is removed.
+    ///
+    /// If the deque is sorted, this removes all duplicates.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq!["foo", "bar", "Bar", "baz", "bar"];
+    ///
+    /// deq.dedup_by(|a, b| a.eq_ignore_ascii_case(b));
+    ///
+    /// assert_eq!(deq, ["foo", "bar", "baz", "bar"]);
+    /// # }
+    /// ```
     #[inline]
     pub fn dedup_by<F>(&mut self, same_bucket: F)
     where
@@ -472,6 +646,65 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.dedup_by(same_bucket);
     }
 
+    /// Creates an iterator which uses a closure to determine if an element
+    /// should be removed.
+    ///
+    /// If the closure returns `true`, then the element is removed and yielded.
+    /// If the closure returns `false`, it will try again, and call the closure
+    /// on the next element, seeing if it passes the test.
+    ///
+    /// Using this method is equivalent to the following code:
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// # let some_predicate = |x: &mut i32| { *x == 2 || *x == 3 || *x == 6
+    /// # };
+    /// let mut deq = FixedSliceDeque::new(0);
+    /// deq.extend(1..7);
+    /// let mut i = 0;
+    /// while i != deq.len() {
+    ///     if some_predicate(&mut deq[i]) {
+    ///         let val = deq.remove(i);
+    ///     // your code here
+    ///     } else {
+    ///         i += 1;
+    ///     }
+    /// }
+    /// # let mut expected = fsdeq![1, 4, 5];
+    /// # assert_eq!(deq, expected);
+    /// # }
+    /// ```
+    ///
+    /// But `drain_filter` is easier to use. `drain_filter` is also more
+    /// efficient, because it can backshift the elements of the deque in
+    /// bulk.
+    ///
+    /// Note that `drain_filter` also lets you mutate every element in the
+    /// filter closure, regardless of whether you choose to keep or remove
+    /// it.
+    ///
+    ///
+    /// # Examples
+    ///
+    /// Splitting a deque into evens and odds, reusing the original allocation:
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut numbers = fsdeq![1, 2, 3, 4, 5, 6, 8, 9, 11, 13, 14, 15];
+    ///
+    /// let evens = numbers
+    ///     .drain_filter(|x| *x % 2 == 0)
+    ///     .collect::<FixedSliceDeque<_>>();
+    /// let odds = numbers;
+    ///
+    /// assert_eq!(fsdeq![2, 4, 6, 8, 14], evens);
+    /// assert_eq!(evens.capacity(), 5);
+    /// assert_eq!(odds, fsdeq![1, 3, 5, 9, 11, 13, 15]);
+    /// assert_eq!(odds.capacity(), 12);
+    /// # }
+    /// ```
     #[inline]
     pub fn drain_filter<F>(&mut self, filter: F) -> slice_deque::DrainFilter<T, F>
     where
@@ -482,11 +715,47 @@ impl<T> FixedSliceDeque<T> {
 }
 
 impl<T: PartialEq> FixedSliceDeque<T> {
+    /// Removes consecutive repeated elements in the deque.
+    ///
+    /// If the deque is sorted, this removes all duplicates.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 2, 3, 2];
+    ///
+    /// deq.dedup();
+    /// assert_eq!(deq, [1, 2, 3, 2]);
+    ///
+    /// deq.sort();
+    /// assert_eq!(deq, [1, 2, 2, 3]);
+    ///
+    /// deq.dedup();
+    /// assert_eq!(deq, [1, 2, 3]);
+    /// # }
+    /// ```
     #[inline]
     pub fn dedup(&mut self) {
         self.buffer.dedup();
     }
 
+    /// Removes the first instance of `item` from the deque if the item exists.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 3, 1];
+    ///
+    /// deq.remove_item(&1);
+    /// assert_eq!(deq, &[2, 3, 1]);
+    /// deq.remove_item(&1);
+    /// assert_eq!(deq, &[2, 3]);
+    /// # }
+    /// ```
     #[inline]
     pub fn remove_item(&mut self, item: &T) -> Option<T> {
         self.buffer.remove_item(item)
@@ -494,24 +763,94 @@ impl<T: PartialEq> FixedSliceDeque<T> {
 }
 
 impl<T: Clone> FixedSliceDeque<T> {
+    /// Clones and appends all elements in a slice to the [`FixedSliceDeque`]
+    /// Capacity is upgraded so elements can fit.
+    /// Iterates over the slice `other`, clones each element, and then appends
+    /// it to this `SliceDeque`. The `other` slice is traversed in-order.
+    ///
+    /// Note that this function is same as `extend` except that it is
+    /// specialized to work with slices instead. If and when Rust gets
+    /// specialization this function will likely be deprecated (but still
+    /// available).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(1);
+    /// assert_eq!(deq.capacity(), 1);
+    /// deq.push_back(1);
+    /// deq.extend_from_slice(&[2, 3, 4]);
+    /// assert_eq!(deq, [1, 2, 3, 4]);
+    /// assert_eq!(deq.capacity(), 4);
+    /// ```
     #[inline]
     pub fn extend_from_slice(&mut self, other: &[T]) {
-        self.buffer.extend_from_slice(other);
         self.capacity = self.len() + other.len();
+        self.buffer.extend_from_slice(other);
     }
 
+    /// Modifies the [`FixedSliceDeque`] in-place so that `capacity` is equal to
+    /// `new_capacity`, either by removing excess elements or by appending clones of
+    /// `value` to the back.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![5, 10, 15];
+    /// assert_eq!(deq, [5, 10, 15]);
+    /// assert_eq!(deq.capacity(), 3);
+    ///
+    /// deq.resize(2, 0);
+    /// assert_eq!(deq, [5, 10]);
+    ///assert_eq!(deq.capacity(), 2);
+    ///
+    /// deq.resize(5, 20);
+    /// assert_eq!(deq, [5, 10, 20, 20, 20]);
+    /// assert_eq!(deq.capacity(), 5);
+    /// # }
+    /// ```
     #[inline]
-    pub fn resize(&mut self, new_len: usize, value: T) {
-        self.capacity = new_len;
-        self.buffer.resize(new_len, value);
+    pub fn resize(&mut self, new_capacity: usize, value: T) {
+        self.capacity = new_capacity;
+        self.buffer.resize(new_capacity, value);
     }
 }
 
 impl<T: Default> FixedSliceDeque<T> {
+    /// Resizes the [`FixedSliceDeque`] in-place so that `capacity` is equal to `new_capacity`.
+    ///
+    /// If `new_capacity` is greater than `capacity`, the [`FixedSliceDeque`] is extended by the
+    /// difference, with each additional slot filled with `Default::default()`.
+    /// If `new_capacity` is less than `len`, the `[`FixedSliceDeque`] is simply truncated.
+    ///
+    /// This method uses `Default` to create new values on every push. If
+    /// you'd rather `Clone` a given value, use [`resize`].
+    ///
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 3];
+    /// deq.resize_default(5);
+    /// assert_eq!(deq, [1, 2, 3, 0, 0]);
+    /// assert_eq!(deq.capacity(), 5);
+    ///
+    /// deq.resize_default(2);
+    /// assert_eq!(deq, [1, 2]);
+    /// assert_eq!(deq.capacity(), 2);
+    /// # }
+    /// ```
+    ///
+    /// [`resize`]: #method.resize
     #[inline]
-    pub fn resize_default(&mut self, new_len: usize) {
-        self.capacity = new_len;
-        self.buffer.resize_default(new_len);
+    pub fn resize_default(&mut self, new_capacity: usize) {
+        self.capacity = new_capacity;
+        self.buffer.resize_default(new_capacity);
     }
 }
 
@@ -574,7 +913,14 @@ impl<'a, T> IntoIterator for &'a mut FixedSliceDeque<T> {
 impl<T> Extend<T> for FixedSliceDeque<T> {
     #[inline]
     fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
-        self.buffer.extend(iter)
+        self.buffer.extend(iter);
+        self.capacity = self.buffer.len();
+    }
+}
+
+impl<T> std::iter::FromIterator<T> for FixedSliceDeque<T> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        Self::from_slice_deque(SliceDeque::from_iter(iter.into_iter()))
     }
 }
 
