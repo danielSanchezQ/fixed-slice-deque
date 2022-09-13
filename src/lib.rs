37b44220630345ed8f309ef3d68201d21b4131ad
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2022-09-13 08:14:08 +0200

    Start adding documentation to methods

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::slice;
 #[derive(Debug, Clone, Eq, Hash)]
 pub struct FixedSliceDeque<T> {
     buffer: SliceDeque<T>,
-    size: usize,
+    capacity: usize,
 }
 
 /// Creates a [`FixedSliceDeque`] containing the arguments.
@@ -94,7 +94,7 @@ impl<T> FixedSliceDeque<T> {
     pub fn new(size: usize) -> Self {
         Self {
             buffer: SliceDeque::with_capacity(size),
-            size,
+            capacity: size,
         }
     }
 
@@ -110,7 +110,7 @@ impl<T> FixedSliceDeque<T> {
     /// ```
     pub fn from_slice_deque(deque: SliceDeque<T>) -> Self {
         Self {
-            size: deque.len(),
+            capacity: deque.len(),
             buffer: deque,
         }
     }
@@ -127,7 +127,7 @@ impl<T> FixedSliceDeque<T> {
     /// ```
     #[inline]
     pub fn is_full(&self) -> bool {
-        self.buffer.len() == self.size
+        self.buffer.len() == self.capacity
     }
 
     /// Is the ring buffer empty ?
@@ -146,54 +146,152 @@ impl<T> FixedSliceDeque<T> {
         self.buffer.is_empty()
     }
 
+    /// Buffer occupied size
     #[inline]
     pub fn len(&self) -> usize {
         self.buffer.len()
     }
 
+    /// Extracts a slice containing the entire deque.
     #[inline]
     pub fn as_slice(&self) -> &[T] {
         self.buffer.as_slice()
     }
 
+    /// Extracts a mutable slice containing the entire deque.
     #[inline]
     pub fn as_mut_slice(&mut self) -> &mut [T] {
         self.buffer.as_mut_slice()
     }
 
+    /// Moves all the elements of `other` into `Self`, leaving `other` empty.
+    ///
+    /// It keeps the internal [`FixedSliceDeque`] capacity. All items that will not fit into the
+    /// capacity are truncated from the head.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::{FixedSliceDeque, fsdeq};
+    /// # fn main() {
+    /// let mut deq = fsdeq![1, 2, 3];
+    /// let mut deq2 = fsdeq![4, 5, 6];
+    /// deq.append(&mut deq2);
+    /// assert_eq!(deq, [4, 5, 6]);
+    /// assert_eq!(deq2, []);
+    /// # }
+    /// ```
     #[inline]
     pub fn append(&mut self, other: &mut Self) {
-        if other.len() + self.len() <= self.size {
+        if other.len() + self.len() <= self.capacity {
             self.buffer.append(&mut other.buffer);
         } else {
             other
                 .buffer
-                .truncate_back(other.len() - (self.size - self.len()));
+                .truncate_back(other.len() - (self.capacity - self.len()));
+            self.buffer.clear();
             self.buffer.append(&mut other.buffer);
         }
         other.buffer.clear();
     }
 
+    /// Provides a reference to the first element, or `None` if the deque is
+    /// empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(3);
+    /// assert_eq!(deq.front(), None);
+    ///
+    /// deq.push_back(1);
+    /// deq.push_back(2);
+    /// assert_eq!(deq.front(), Some(&1));
+    /// deq.push_front(3);
+    /// assert_eq!(deq.front(), Some(&3));
+    /// ```
     #[inline]
     pub fn front(&self) -> Option<&T> {
         self.buffer.front()
     }
 
+    /// Provides a mutable reference to the first element, or `None` if the
+    /// deque is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(3);
+    /// assert_eq!(deq.front(), None);
+    ///
+    /// deq.push_back(1);
+    /// deq.push_back(2);
+    /// assert_eq!(deq.front(), Some(&1));
+    /// (*deq.front_mut().unwrap()) = 3;
+    /// assert_eq!(deq.front(), Some(&3));
+    /// ```
     #[inline]
     pub fn front_mut(&mut self) -> Option<&mut T> {
         self.buffer.front_mut()
     }
 
+    /// Provides a reference to the last element, or `None` if the deque is
+    /// empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(3);
+    /// assert_eq!(deq.back(), None);
+    ///
+    /// deq.push_back(1);
+    /// deq.push_back(2);
+    /// assert_eq!(deq.back(), Some(&2));
+    /// deq.push_front(3);
+    /// assert_eq!(deq.back(), Some(&2));
+    /// ```
     #[inline]
     pub fn back(&self) -> Option<&T> {
         self.buffer.back()
     }
 
+    /// Provides a mutable reference to the last element, or `None` if the
+    /// deque is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(3);
+    /// assert_eq!(deq.front(), None);
+    ///
+    /// deq.push_back(1);
+    /// deq.push_back(2);
+    /// assert_eq!(deq.back(), Some(&2));
+    /// (*deq.back_mut().unwrap()) = 3;
+    /// assert_eq!(deq.back(), Some(&3));
+    /// ```
     #[inline]
     pub fn back_mut(&mut self) -> Option<&mut T> {
         self.buffer.back_mut()
     }
 
+    /// Prepends `value` to the deque.
+    /// If the deque `is_full` the last item is discarded and returned
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(1);
+    /// deq.push_front(1);
+    /// let pop = deq.push_front(2);
+    /// assert_eq!(pop, Some(1));
+    /// assert_eq!(deq.front(), Some(&2));
+    /// ```
     #[inline]
     pub fn push_front(&mut self, value: T) -> Option<T> {
         let ret = self.is_full().then(|| self.buffer.pop_back()).flatten();
@@ -201,6 +299,19 @@ impl<T> FixedSliceDeque<T> {
         ret
     }
 
+    /// Appends `value` to the deque.
+    /// If the deque `is_full` the first item is discarded and returned
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(1);
+    /// deq.push_back(1);
+    /// let pop = deq.push_back(3);
+    /// assert_eq!(pop, Some(1));
+    /// assert_eq!(deq.back(), Some(&3));
+    /// ```
     #[inline]
     pub fn push_back(&mut self, value: T) -> Option<T> {
         let ret = self.is_full().then(|| self.buffer.pop_front()).flatten();
@@ -208,6 +319,17 @@ impl<T> FixedSliceDeque<T> {
         ret
     }
 
+    /// Prepends `value` to the deque if the deque is `!is_full` (not full)
+    /// otherwise an error with the passed value is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(0);
+    /// let res = deq.try_push_front(1);
+    /// assert_eq!(res, Err(1));
+    /// ```
     #[inline]
     pub fn try_push_front(&mut self, value: T) -> Result<(), T> {
         if self.is_full() {
@@ -217,6 +339,17 @@ impl<T> FixedSliceDeque<T> {
         Ok(())
     }
 
+    /// Appends `value` to the deque if the deque is `!is_full` (not full)
+    /// otherwise an error with the passed value is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(0);
+    /// let res = deq.try_push_back(1);
+    /// assert_eq!(res, Err(1));
+    /// ```
     #[inline]
     pub fn try_push_back(&mut self, value: T) -> Result<(), T> {
         if self.is_full() {
@@ -226,11 +359,45 @@ impl<T> FixedSliceDeque<T> {
         Ok(())
     }
 
+    /// Removes the first element and returns it, or `None` if the deque is
+    /// empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(2);
+    /// assert_eq!(deq.pop_front(), None);
+    ///
+    /// deq.push_back(1);
+    /// deq.push_back(2);
+    ///
+    /// assert_eq!(deq.pop_front(), Some(1));
+    /// assert_eq!(deq.pop_front(), Some(2));
+    /// assert_eq!(deq.pop_front(), None);
+    /// ```
     #[inline]
     pub fn pop_front(&mut self) -> Option<T> {
         self.buffer.pop_front()
     }
 
+    /// Removes the last element from the deque and returns it, or `None` if it
+    /// is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(2);
+    /// assert_eq!(deq.pop_back(), None);
+    ///
+    /// deq.push_back(1);
+    /// deq.push_back(3);
+    ///
+    /// assert_eq!(deq.pop_back(), Some(3));
+    /// assert_eq!(deq.pop_back(), Some(1));
+    /// assert_eq!(deq.pop_back(), None);
+    /// ```
     #[inline]
     pub fn pop_back(&mut self) -> Option<T> {
         self.buffer.pop_back()
@@ -238,12 +405,12 @@ impl<T> FixedSliceDeque<T> {
 
     #[inline]
     pub fn truncate_front(&mut self, len: usize) {
-        self.buffer.truncate_front(len.min(self.size));
+        self.buffer.truncate_front(len.min(self.capacity));
     }
 
     #[inline]
     pub fn truncate_back(&mut self, len: usize) {
-        self.buffer.truncate_back(len.min(self.size));
+        self.buffer.truncate_back(len.min(self.capacity));
     }
 
     #[inline]
@@ -330,12 +497,12 @@ impl<T: Clone> FixedSliceDeque<T> {
     #[inline]
     pub fn extend_from_slice(&mut self, other: &[T]) {
         self.buffer.extend_from_slice(other);
-        self.size = self.len() + other.len();
+        self.capacity = self.len() + other.len();
     }
 
     #[inline]
     pub fn resize(&mut self, new_len: usize, value: T) {
-        self.size = new_len;
+        self.capacity = new_len;
         self.buffer.resize(new_len, value);
     }
 }
@@ -343,7 +510,7 @@ impl<T: Clone> FixedSliceDeque<T> {
 impl<T: Default> FixedSliceDeque<T> {
     #[inline]
     pub fn resize_default(&mut self, new_len: usize) {
-        self.size = new_len;
+        self.capacity = new_len;
         self.buffer.resize_default(new_len);
     }
 }
@@ -371,7 +538,7 @@ impl<T> From<SliceDeque<T>> for FixedSliceDeque<T> {
 impl<'a, T: Clone> From<&'a [T]> for FixedSliceDeque<T> {
     fn from(slice: &'a [T]) -> Self {
         Self {
-            size: slice.len(),
+            capacity: slice.len(),
             buffer: SliceDeque::from(slice),
         }
     }
@@ -380,7 +547,7 @@ impl<'a, T: Clone> From<&'a [T]> for FixedSliceDeque<T> {
 impl<'a, T: Clone> From<&'a mut [T]> for FixedSliceDeque<T> {
     fn from(slice: &'a mut [T]) -> Self {
         Self {
-            size: slice.len(),
+            capacity: slice.len(),
             buffer: SliceDeque::from(slice),
         }
     }
@@ -453,7 +620,6 @@ array_impls! {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
     fn is_full() {}
