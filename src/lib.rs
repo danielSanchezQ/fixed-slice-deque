2f342a1d37743a020d5d00504da16196628a6192
danielsanchezq <sanchez.quiros.daniel@gmail.com>
2022-09-13 07:50:51 +0200

    Added remaining derive impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,95 @@
 use slice_deque::SliceDeque;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
+use std::slice;
 
+#[derive(Debug, Clone, Eq, Hash)]
 pub struct FixedSliceDeque<T> {
     buffer: SliceDeque<T>,
     size: usize,
 }
 
+/// Creates a [`FixedSliceDeque`] containing the arguments.
+///
+/// `fsdeq!` allows `SliceDeque`s to be defined with the same syntax as array
+/// expressions. There are two forms of this macro:
+///
+/// - Create a [`SliceDeque`] containing a given list of elements:
+///
+/// ```
+/// # #[macro_use] extern crate fixed_slice_deque;
+/// # use fixed_slice_deque::FixedSliceDeque;
+/// # fn main() {
+/// let v: FixedSliceDeque<i32> = fsdeq![1, 2, 3];
+/// assert_eq!(v[0], 1);
+/// assert_eq!(v[1], 2);
+/// assert_eq!(v[2], 3);
+/// # }
+/// ```
+///
+/// - Create a [`SliceDeque`] from a given element and size:
+///
+/// ```
+/// # #[macro_use] extern crate fixed_slice_deque;
+/// # use fixed_slice_deque::FixedSliceDeque;
+/// # fn main() {
+/// let v = fsdeq![7; 3];
+/// assert_eq!(v, [7, 7, 7]);
+/// # }
+/// ```
+///
+/// Note that unlike array expressions this syntax supports all elements
+/// which implement `Clone` and the number of elements doesn't have to be
+/// a constant.
+///
+/// This will use `clone` to duplicate an expression, so one should be careful
+/// using this with types having a nonstandard `Clone` implementation. For
+/// example, `sdeq![Rc::new(1); 5]` will create a deque of five references
+/// to the same boxed integer value, not five references pointing to
+/// independently boxed integers.
+///
+/// ```
+/// # #[macro_use] extern crate fixed_slice_deque;
+/// # use slice_deque::SliceDeque;
+/// # use std::rc::Rc;
+/// # fn main() {
+/// let v = fsdeq![Rc::new(1_i32); 5];
+/// let ptr: *const i32 = &*v[0] as *const i32;
+/// for i in v.iter() {
+///     assert_eq!(Rc::into_raw(i.clone()), ptr);
+/// }
+/// # }
+/// ```
+///
+/// [`SliceDeque`]: struct.SliceDeque.html
+#[macro_export]
+macro_rules! fsdeq {
+    ($elem:expr; $n:expr) => (
+        {
+            let mut deq = $crate::FixedSliceDeque::new($n);
+            deq.resize($n, $elem);
+            deq
+        }
+    );
+    ($($x:expr),*) => (
+        {
+            let sdeq = ::slice_deque::sdeq![$($x),*];
+            let deq = $crate::FixedSliceDeque::from_slice_deque(sdeq);
+            deq
+        }
+    );
+    ($($x:expr,)*) => (fsdeq![$($x),*])
+}
+
 impl<T> FixedSliceDeque<T> {
+    /// Create an empty fixed size deque with capacity to hold `n` elements.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let deq = FixedSliceDeque::new(10);
+    /// # let o: FixedSliceDeque<u32> = deq;
+    /// ```
     #[inline]
     pub fn new(size: usize) -> Self {
         Self {
@@ -15,6 +98,16 @@ impl<T> FixedSliceDeque<T> {
         }
     }
 
+    /// Create an new fixed size deque with capacity to hold `n` elements from a [`SliceDeque`].
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// # use slice_deque::sdeq;
+    /// let deq = FixedSliceDeque::from_slice_deque(sdeq![1, 2, 3]);
+    /// # let o: FixedSliceDeque<u32> = deq;
+    /// ```
     pub fn from_slice_deque(deque: SliceDeque<T>) -> Self {
         Self {
             size: deque.len(),
@@ -22,11 +115,32 @@ impl<T> FixedSliceDeque<T> {
         }
     }
 
+    /// Is the ring buffer full ?
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(10);
+    /// assert!(!deq.is_full());
+    /// # let o: FixedSliceDeque<u32> = deq;
+    /// ```
     #[inline]
     pub fn is_full(&self) -> bool {
         self.buffer.len() == self.size
     }
 
+    /// Is the ring buffer empty ?
+    /// Check if the buffer do not contain any item
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use fixed_slice_deque::FixedSliceDeque;
+    /// let mut deq = FixedSliceDeque::new(10);
+    /// assert!(deq.is_empty());
+    /// # let o: FixedSliceDeque<u32> = deq;
+    /// ```
     #[inline]
     pub fn is_empty(&self) -> bool {
         self.buffer.is_empty()
@@ -205,6 +319,33 @@ impl<T: PartialEq> FixedSliceDeque<T> {
     pub fn dedup(&mut self) {
         self.buffer.dedup();
     }
+
+    #[inline]
+    pub fn remove_item(&mut self, item: &T) -> Option<T> {
+        self.buffer.remove_item(item)
+    }
+}
+
+impl<T: Clone> FixedSliceDeque<T> {
+    #[inline]
+    pub fn extend_from_slice(&mut self, other: &[T]) {
+        self.buffer.extend_from_slice(other);
+        self.size = self.len() + other.len();
+    }
+
+    #[inline]
+    pub fn resize(&mut self, new_len: usize, value: T) {
+        self.size = new_len;
+        self.buffer.resize(new_len, value);
+    }
+}
+
+impl<T: Default> FixedSliceDeque<T> {
+    #[inline]
+    pub fn resize_default(&mut self, new_len: usize) {
+        self.size = new_len;
+        self.buffer.resize_default(new_len);
+    }
 }
 
 impl<T> Deref for FixedSliceDeque<T> {
@@ -215,10 +356,99 @@ impl<T> Deref for FixedSliceDeque<T> {
     }
 }
 
+impl<T> DerefMut for FixedSliceDeque<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.buffer.as_mut_slice()
+    }
+}
+
 impl<T> From<SliceDeque<T>> for FixedSliceDeque<T> {
     fn from(deque: SliceDeque<T>) -> Self {
         Self::from_slice_deque(deque)
     }
+}
+
+impl<'a, T: Clone> From<&'a [T]> for FixedSliceDeque<T> {
+    fn from(slice: &'a [T]) -> Self {
+        Self {
+            size: slice.len(),
+            buffer: SliceDeque::from(slice),
+        }
+    }
+}
+
+impl<'a, T: Clone> From<&'a mut [T]> for FixedSliceDeque<T> {
+    fn from(slice: &'a mut [T]) -> Self {
+        Self {
+            size: slice.len(),
+            buffer: SliceDeque::from(slice),
+        }
+    }
+}
+
+impl<'a, T> IntoIterator for &'a FixedSliceDeque<T> {
+    type Item = &'a T;
+    type IntoIter = slice::Iter<'a, T>;
+    #[inline]
+    fn into_iter(self) -> slice::Iter<'a, T> {
+        self.iter()
+    }
+}
+
+impl<'a, T> IntoIterator for &'a mut FixedSliceDeque<T> {
+    type Item = &'a mut T;
+    type IntoIter = slice::IterMut<'a, T>;
+    #[inline]
+    fn into_iter(self) -> slice::IterMut<'a, T> {
+        self.iter_mut()
+    }
+}
+
+impl<T> Extend<T> for FixedSliceDeque<T> {
+    #[inline]
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        self.buffer.extend(iter)
+    }
+}
+
+macro_rules! __impl_slice_eq1 {
+    ($Lhs:ty, $Rhs:ty) => {
+        __impl_slice_eq1! { $Lhs, $Rhs, Sized }
+    };
+    ($Lhs:ty, $Rhs:ty, $Bound:ident) => {
+        impl<'a, 'b, A: $Bound, B> PartialEq<$Rhs> for $Lhs
+        where
+            A: PartialEq<B>,
+        {
+            #[inline]
+            fn eq(&self, other: &$Rhs) -> bool {
+                self.buffer[..] == other[..]
+            }
+        }
+    };
+}
+
+__impl_slice_eq1! { FixedSliceDeque<A>, FixedSliceDeque<B> }
+__impl_slice_eq1! { FixedSliceDeque<A>, SliceDeque<B> }
+__impl_slice_eq1! { FixedSliceDeque<A>, &'b [B] }
+__impl_slice_eq1! { FixedSliceDeque<A>, &'b mut [B] }
+__impl_slice_eq1! { FixedSliceDeque<A>, Vec<B> }
+
+macro_rules! array_impls {
+    ($($N: expr)+) => {
+        $(
+            // NOTE: some less important impls are omitted to reduce code bloat
+            __impl_slice_eq1! { FixedSliceDeque<A>, [B; $N] }
+            __impl_slice_eq1! { FixedSliceDeque<A>, &'b [B; $N] }
+        )+
+    }
+}
+
+array_impls! {
+    0  1  2  3  4  5  6  7  8  9
+    10 11 12 13 14 15 16 17 18 19
+    20 21 22 23 24 25 26 27 28 29
+    30 31 32
 }
 
 #[cfg(test)]
